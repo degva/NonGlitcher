08cf9d48d31cb7bc02bd72ef4f47ee01d08c9e14
Diego Vargas <degva.29@gmail.com>
2015-10-05 15:01:01 -0500

    Last updates

--- libs/tbanco.c
+++ libs/tbanco.c
@@ -1,5 +1,6 @@
 #include "tbanco.h"
 #include "tpersona.h"
+#include "tarray.h"
 
 float calc_credit_a (TArr * data, TPersona persona) {
   return 0;
@@ -17,34 +18,16 @@ void print_person (TPersona * persona, float credit) {
 
 }
 
-void start (TArr  *data, const char * filepath, const char * delimiter) {
-  printf("Leyendo archivo de entrada...");
+void start (TArr *data, TArr *personas) {
+  printf("Determinando el credito para las personas con el banco A\n");
+  for (i=0; i<personas->len; i++)
+    calc_credit_a(data, t_array_index(personas, i));
   
-  TArr *people;
-  FILE *f;
-  char line[1000];
-
-  people = t_array_new();
-
-  f = fopen(filepath, 'r');
-
-  if (!f)
-    goto handle_error;
-
-  while (fgets (line, 1000, f)) {
-    Persona *persona;
-    persona = persona_from_string(line, delimiter);
-    if (!persona)
-      goto handle_error;
-    t_array_append(people, persona);
-  }
-  fclose(f);
-
-  if (people->len == 0)
-    goto handle_error;
-
-  return people;
-handle_error:
-  free(people);
-  return NULL;
+  printf("Determinando el credito para las personas con el banco B\n");
+  for (i=0; i<personas->len; i++)
+    calc_credit_b(data, t_array_index(personas, i));
+  
+  printf("Determinando el credito para las personas con el banco C\n");
+  for (i=0; i<personas->len; i++)
+    calc_credit_c(data, t_array_index(personas, i));
 }
